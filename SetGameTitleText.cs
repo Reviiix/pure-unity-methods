57b1df316608b12ca4cdc6b34e4d4a766ba01288
Leon Higgin <admin@leonmichaelhiggin>
2021-06-07 17:36:23 +0100

    SetText

---
+++ SetGameTitleText.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace PureFunctions
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class SetGameTitleText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TMP_Text>().text = PlayerSettings.productName.ToUpper();
+            Destroy(GetComponent<SetGameTitleText>());
+        }
+    }
+}
---
+++ SetVersionNumberText.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace PureFunctions
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class SetVersionNumberText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TMP_Text>().text = "V" + PlayerSettings.bundleVersion + ".";
+            Destroy(GetComponent<SetVersionNumberText>());
+        }
+    }
+}
+
