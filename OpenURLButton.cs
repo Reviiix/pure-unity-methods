27d78f53bd2aa5d76ba0371ede094f38974c4f03
Leon Higgin <admin@leonmichaelhiggin>
2021-06-07 17:36:52 +0100

    open URL

---
+++ OpenURLButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PureFunctions
+{
+    [RequireComponent(typeof(Button))]
+    public class OpenURLButton : MonoBehaviour
+    {
+        [SerializeField] private string url;
+        private Button _button;
+
+        private void Awake()
+        {
+            AssignButtonEvents();
+        }
+    
+        private void AssignButtonEvents()
+        {
+            GetComponent<Button>().onClick.AddListener(OpenURLButtonPressed);
+        }
+
+        private void OpenURLButtonPressed()
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
