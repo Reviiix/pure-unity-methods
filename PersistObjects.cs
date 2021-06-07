ffec6f8879faae2f05610c2f853ccfb8affb238c
Leon Higgin <admin@leonmichaelhiggin>
2021-06-07 17:36:35 +0100

    peresist objects

---
+++ PersistObjects.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace PureFunctions
+{
+    public class PersistObjects : MonoBehaviour
+    {
+        [SerializeField] private Transform[] persistantObjects;
+    
+        private void Awake()
+        {
+            SetObjectsToPersistAcrossScenes();
+        }
+
+        private void SetObjectsToPersistAcrossScenes()
+        {
+            foreach (var objectToPersist in persistantObjects)
+            {
+                DontDestroyOnLoad(objectToPersist);
+            }
+            Destroy(GetComponent<PersistObjects>());
+        }
+    }
+}
