2fcc2244b91803c41b40701c5fc143b559ac3ec5
Leon Higgin <admin@leonmichaelhiggin>
2021-06-07 17:35:22 +0100

    movement with job system

---
+++ Movement/MoveInAndOutOfView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PureFunctions.Movement
+{
+    public static class MoveInAndOutOfView
+    {
+        public static IEnumerator MoveTransformToCentre(Transform transformToMove, int speed, int distanceTolerance = 1, Action callBack = null)
+        {
+            while (Vector2.Distance(transformToMove.localPosition, Vector2.zero) >= distanceTolerance)
+            {
+                MoveToCentre(transformToMove, speed);
+                yield return null;
+            }
+            callBack?.Invoke();
+        }
+        
+        public static IEnumerator MoveTransformToLeft(Transform transformToMove, int speed, Vector3 leftPosition, int distanceTolerance = 1, Action callBack = null)
+        {
+            while (Vector2.Distance(transformToMove.localPosition, -leftPosition) >= distanceTolerance)
+            {
+                MoveToLeft(transformToMove, speed, (int)leftPosition.x);
+                yield return null;
+            }
+            callBack?.Invoke();
+        }
+        
+        private static void MoveToCentre(Transform objectToMove, int speed)
+        {
+            MovementJobs.MoveObjects(new []{objectToMove}, new []{Vector3.zero}, speed);
+        }
+        
+        private static void MoveToLeft(Transform objectToMove, int speed, int leftDistance)
+        {
+            MovementJobs.MoveObjects(new []{objectToMove}, new []{new Vector3(-leftDistance,0,0)}, speed);
+        }
+    }
+}
---
+++ Movement/MovementJobs.cs
@@ -0,0 +1,66 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace PureFunctions.Movement
+{
+    //The Job system requires a lot of boiler plate code but is the most optimised development strategy in Unity 2020.
+    public static class MovementJobs
+    {
+        public static void MoveObjects(Transform[] objectsToMove, Vector3[] positions, float speed)
+        {
+            //Create primitive/native variables to pass to job.
+            var amountOfObjects = objectsToMove.Length;
+            var objectPositions = new NativeArray<float3>(amountOfObjects, Allocator.TempJob);
+            var endPositions = new NativeArray<float3>(amountOfObjects, Allocator.TempJob);
+            
+            //Compose variables.
+            for (var i = 0; i < amountOfObjects; i++)
+            {
+                objectPositions[i] = new float3(objectsToMove[i].localPosition);
+                endPositions[i]= new float3(positions[i]);
+            }
+            
+            //Create job handle and pass in values.
+            var jobHandle = new MoveMultipleObjectsTowardsPosition
+            {
+                PositionsOfObjectsToMove = objectPositions,
+                PositionsToMoveTo = endPositions,
+                Speed = speed,
+                DeltaTime = Time.deltaTime,
+            };
+
+            //Run the job with the handle.
+            var handle = jobHandle.Schedule(amountOfObjects, 10);
+            handle.Complete();
+    
+            //Reassign the position values (Having had the job applied).
+            for (var i = 0; i < amountOfObjects; i++)
+            {
+                objectsToMove[i].transform.localPosition = objectPositions[i];
+            }
+    
+            //Dispose of native arrays (Not handled by garbage collector).
+            objectPositions.Dispose();
+            endPositions.Dispose();
+        }
+
+        [BurstCompile]
+        private struct MoveMultipleObjectsTowardsPosition : IJobParallelFor
+        {
+            public NativeArray<float3> PositionsOfObjectsToMove;
+            public NativeArray<float3> PositionsToMoveTo;
+            public float Speed;
+            public float DeltaTime;
+
+            public void Execute(int index)
+            {
+                var directionOfTravel = PositionsToMoveTo[index] - PositionsOfObjectsToMove[index];
+                PositionsOfObjectsToMove[index] += new float3(directionOfTravel.x * Speed * DeltaTime, directionOfTravel.y * Speed * DeltaTime, directionOfTravel.z * Speed * DeltaTime);
+            }
+        }
+  
+    }
+}
