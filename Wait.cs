c7e0ad9af7897f5c39187cb7d86946e4af8804c6
Leon Higgin <admin@leonmichaelhiggin>
2021-06-07 17:36:11 +0100

    wait

---
+++ Wait.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PureFunctions
+{
+   public static class Wait
+   {
+      public static IEnumerator WaitThenCallBack(float seconds, Action callBack)
+      {
+         yield return new WaitForSeconds(seconds);
+         callBack();
+      }
+   }
+}
