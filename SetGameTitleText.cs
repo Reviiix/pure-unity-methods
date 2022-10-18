7c290ba79d78bfe3cacef19b199e3dfe765ef78f
Leon Higgin <admin@leonmichaelhiggin>
2022-10-18 21:31:37 +0100

    Capitalise option in SetGameTitleText

--- SetGameTitleText.cs
+++ SetGameTitleText.cs
@@ -7,9 +7,11 @@ namespace PureFunctions
     [RequireComponent(typeof(TMP_Text))]
     public class SetGameTitleText : MonoBehaviour
     {
+        [SerializeField] private bool capitalise;
+        
         private void Awake()
         {
-            GetComponent<TMP_Text>().text = PlayerSettings.productName.ToUpper();
+            GetComponent<TMP_Text>().text = capitalise ? PlayerSettings.productName.ToUpper() : PlayerSettings.productName;
             Destroy(GetComponent<SetGameTitleText>());
         }
     }
