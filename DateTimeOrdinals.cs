01c0558813575d45d2e25893a202a9e6666efeaa
Leon Higgin <admin@leonmichaelhiggin>
2022-10-22 02:23:43 +0100

    Restore date time ordinals

---
+++ DateTimeOrdinals.cs
@@ -0,0 +1,30 @@
+namespace PureFunctions.DateTime
+{
+    public static class DateTimeOrdinals
+    {
+        public enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+        
+        public enum Months { January, February, March, April, May, June, July, August, September, October, November, December }
+
+        public static string ConvertToOrdinalString(int number)
+        {
+            if (number <= 0) return number.ToString();
+
+            return (number % 100) switch
+            {
+                11 => number + "th",
+                12 => number + "th",
+                13 => number + "th",
+                _ => (number % 10) switch
+                {
+                    1 => number + "st",
+                    2 => number + "nd",
+                    3 => number + "rd",
+                    _ => number + "th"
+                }
+            };
+        }
+    }
+}
+
+
