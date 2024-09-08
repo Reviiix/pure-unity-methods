36423ad850646a06cd561573b28778f943791235
Leon Higgin <admin@leonmichaelhiggin>
2024-09-09 00:27:56 +0100

    Flash sprite

---
+++ UnitySpecific/Effects/FlashSprite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class FlashSprite
+{
+    public static IEnumerator Flash(SpriteRenderer renderer, float cycles, WaitForSeconds flashTime, Action completionCallBack)
+    {
+        for (var i = 0; i < cycles; i++)
+        {
+            renderer.enabled = !renderer.enabled;
+            yield return flashTime;
+        }
+        renderer.enabled = true;
+        completionCallBack();
+    }
+}
