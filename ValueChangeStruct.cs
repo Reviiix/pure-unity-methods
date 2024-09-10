b596811ea2d36382a689f6cd3810ff46de358e0c
Leon Higgin <admin@leonmichaelhiggin>
2024-09-10 16:57:24 +0100

    value change struct

--- ValueChangeStruct.cs
+++ ValueChangeStruct.cs
@@ -3,12 +3,12 @@ namespace PureFunctions
     /// <summary>
     /// This ValueChangeInformation struct is just an alternative to using key value pairs or tuples or something that give greater control/descriptions of the data
     /// </summary>
-    public readonly struct ValueChangeInformation
+    public readonly struct ValueChangeStruct
     {
         public long OldValue { get; }
         public  long NewValue { get; }
 
-        public ValueChangeInformation(long oldValue, long newValue)
+        public ValueChangeStruct(long oldValue, long newValue)
         {
             OldValue = oldValue;
             NewValue = newValue;
