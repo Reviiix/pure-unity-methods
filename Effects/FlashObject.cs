8111cd7f0ea1c0fbe9f24424866ea50b3b20fe60
Leon Higgin <admin@leonmichaelhiggin.co.uk>
2024-10-11 21:54:40 +0100

    FlashObject.cs: Use parameters instead of global variables to improve usability

--- Effects/FlashObject.cs
+++ Effects/FlashObject.cs
@@ -11,26 +11,23 @@ namespace pure_unity_methods.Effects
     /// </summary>
     public static class FlashObject
     {
-        private const float FlashTime = 0.1f;
-        private static readonly WaitForSeconds WaitFlashTime = new WaitForSeconds(FlashTime);
-
-        public static IEnumerator FlashGraphic(Graphic renderer, float cycles, Action completionCallBack = null)
+        public static IEnumerator FlashGraphic(Graphic renderer, float cycles, WaitForSeconds waitFlashTime, Action completionCallBack = null)
         {
             for (var i = 0; i < cycles; i++)
             {
                 renderer.enabled = !renderer.enabled;
-                yield return WaitFlashTime;
+                yield return waitFlashTime;
             }
             renderer.enabled = true;
             completionCallBack?.Invoke();
         }
         
-        public static IEnumerator FlashGameObject(GameObject objectToFlash, float cycles, Action completionCallBack = null)
+        public static IEnumerator FlashGameObject(GameObject objectToFlash, float cycles, WaitForSeconds waitFlashTime, Action completionCallBack = null)
         {
             for (var i = 0; i < cycles; i++)
             {
                 objectToFlash.SetActive(!objectToFlash.activeInHierarchy);
-                yield return WaitFlashTime;
+                yield return waitFlashTime;
             }
             objectToFlash.SetActive(true);
             completionCallBack?.Invoke();
